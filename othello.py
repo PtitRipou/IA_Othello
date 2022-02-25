3001782002d3e758d2bbf9ab9d77c6c8095eacc1
Wassim Belaiba <wassbelaiba@gmail.com>
2022-02-25 13:39:22 +0100

    [fix]_affichage tableau

--- othello.py
+++ othello.py
@@ -8,3 +8,15 @@ def init_othello():
     liste_pos[3][3], liste_pos[4][4] = 1, 1
     liste_pos[3][4], liste_pos[4][3] = 2, 2
     return liste_pos
+
+# Permets la gestion de l'affichage
+def display_othello(board: list[int]):
+    print("   a  b  c  d  e  f  g  h")
+    k = 1
+    for i in board:
+        print(k,i)
+        k+=1
+
+
+liste = init_othello()
+display_othello(liste)
