8c9a869bec7f05f32f628e6797a633c6ca8724fb
Baptiste Guesnay <baptiste.guesnay41@gmail.com>
2022-03-02 09:56:55 +0100

    Changement de nom othello to othello_board

--- gameplay.py
+++ gameplay.py
@@ -1,1 +1,26 @@
 #Implémentation des instrunctions de jeu
+
+#Permet de déterminer les positions suivantes pour une configuration de plateau
+def next_pos(board : 'list[list[int]]', player : 'str'):
+    
+    next_board = board.copy
+    player_int = 0
+
+    if player == "player1" :
+        player_int = 1
+    elif player == "player2" :
+        player_int = 2
+    else :
+        print("Error : Not a valid player !")
+    
+    for k in range(8):
+        for p in range(8):
+            if board[k][p] == player_int :
+                if exist_neighbor(board, k, p) == True:
+                    next_board = 
+
+
+    return next_board
+
+    def exist_neighbor():
+        return
