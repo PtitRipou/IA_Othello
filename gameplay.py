b353f6688bc0b4d0c9b962aa39a6b16c82a00bba
Baptiste Guesnay <baptiste.guesnay41@gmail.com>
2022-03-03 15:02:23 +0100

    fonction next pos terminée

--- gameplay.py
+++ gameplay.py
@@ -1,26 +1,87 @@
 #Implémentation des instrunctions de jeu
 
-#Permet de déterminer les positions suivantes pour une configuration de plateau
+import othello_board as ob
+import copy
+
+#Permet de déterminer les positions suivantes possible pour une configuration de plateau
 def next_pos(board : 'list[list[int]]', player : 'str'):
-    
-    next_board = board.copy
+    next_board = copy.deepcopy(board)
     player_int = 0
 
-    if player == "player1" :
+    if player == "player1":
         player_int = 1
-    elif player == "player2" :
+
+    elif player == "player2":
         player_int = 2
-    else :
+
+    else:
         print("Error : Not a valid player !")
     
     for k in range(8):
         for p in range(8):
-            if board[k][p] == player_int :
-                if exist_neighbor(board, k, p) == True:
-                    next_board = 
 
+            if board[k][p] == player_int :
+                list_play = list_possible_play(board, k, p, player_int)
+                if list_play != []:
+                    ob.display_othello(board)
+                    for i in range(len(list_play)):
+                        next_board[list_play[i][0]][list_play[i][1]] = 3
 
     return next_board
 
-    def exist_neighbor():
-        return
+#Détermine si le pion en p,k permet de jouer un coup (analyse des pions voisins)
+def list_possible_play(board, k, p, player):
+    pos_k = [-1,-1,-1,0,0,1,1,1]
+    pos_p = [-1,0,1,-1,1,-1,0,1]
+    list_play = []
+
+    for i in range(8):
+        k2 = k + pos_k[i]
+        p2 = p + pos_p[i]
+
+        if k2 < 0 or p2 < 0 or k2 > 7 or p2 > 7:
+            continue
+
+        else:
+
+            if board[k2][p2] == 0:
+                continue
+
+            elif board[k2][p2] == player:
+                continue
+
+            else:
+                var = how_far(board, pos_k[i], pos_p[i], k2, p2, player)
+                
+                if var == False:
+                    continue
+
+                else:
+                    list_play.append(var)
+
+    return list_play
+
+#Permet de savoir, lorsqu'un pion est voisin d'un pion adverse, si un coup est jouable
+def how_far(board, ind_k, ind_p, k, p, player):
+    var1 = True
+    var2 = 0
+
+    while var1 == True:
+        var2 += 1
+        k2 = k + ind_k * var2
+        p2 = p + ind_p * var2
+
+        if k2 < 0 or p2 < 0 or k2 > 7 or p2 > 7:
+            break
+
+        elif board[k2][p2] == player:
+            break
+
+        elif board[k2][p2] == 0:
+            return [k2, p2]
+
+    return False
+
+board = ob.init_othello()
+next_board = next_pos(board, 'player1')
+ob.display_othello(next_board)
--- othello_board.py
+++ othello_board.py
@@ -5,14 +5,17 @@ def init_othello():
     liste_pos=[]
     for k in range(8):
         liste_pos.append([0,0,0,0,0,0,0,0])
-    liste_pos[3][3], liste_pos[4][4] = 1, 1
-    liste_pos[3][4], liste_pos[4][3] = 2, 2
+    liste_pos[3][3], liste_pos[4][4] = 2, 2
+    liste_pos[3][4], liste_pos[4][3] = 1, 1
     return liste_pos
 
 # Permets la gestion de l'affichage
-def display_othello(board: 'list[int]'):
+def display_othello(board: 'list[list[int]]'):
     print("   a  b  c  d  e  f  g  h")
     k = 1
     for i in board:
         print(k,i)
         k+=1
+
+board = init_othello()
+display_othello(board)
